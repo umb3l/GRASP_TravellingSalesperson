df6586e3318382411c652cc111b9c69ca2ce0a73
Ule <ule.bezerra@gmail.com>
2018-10-31 15:13:31 -0300

    no message

--- GRASP.c
+++ GRASP.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define INT_MAX 2000;
+#define INT_MAX 200000;
 
 int nElementos = 0;
 
@@ -14,67 +14,67 @@ struct cidade {
 
 //Declaração das funções
 float distancia(struct cidade a, struct cidade b);
+float calculaCusto(float m[][nElementos], int* caminho);
 void printCaminho(int *caminho);
 void construcaoGulosa(float m[][nElementos], int* solucao_inicial);
-void construcaoAleatoria(float m[][nElementos], int* caminho);
 void copiaCaminho(int* origem, int* destino);
-float calculaCusto(float m[][nElementos], int* caminho);
 void swap(float m[][nElementos], int* solucao_inicial, int* melhor_vizinho);
 void twoOpt(float m[][nElementos], int* solucao_inicial, float melhor_custo, int* melhor_vizinho);
 void twoOptSwap(int* entrada, int i, int k, int* saida);
-int VND(float m[][nElementos], float custo_melhor_vizinho, int* melhor_vizinho, int* otimo_local);
-void GRASP(float m[][nElementos], int* solucao_inicial, int* solucao_final);
+void VND(float m[][nElementos], float custo_melhor_vizinho, int* melhor_vizinho, int* otimo_local);
+void GRASP(float m[][nElementos], int* solucao_inicial, int* solucao_final, int alfa);
+void gerarLCR(int* entrada, int a, int* saida);
 
 int main(void) {
+    int t;
+    int alfa = 2;
 
-      int t;
+    //FILE* fp = fopen("dijbouti.txt", "r");
+    //FILE* fp = fopen("bcl380.txt", "r");
+    FILE* fp = fopen("western sahara.txt", "r");
+    //FILE* fp = fopen("xqg237.txt", "r");
+    //FILE* fp = fopen("zimbabwe.txt", "r");
+    //FILE* fp = fopen("pka379.txt", "r");
+    //FILE* fp = fopen("qatar.txt", "r");
+    //FILE* fp = fopen("uruguay.txt", "r");
+    //FILE* fp = fopen("xqg131.txt", "r");
+    //FILE* fp = fopen("pma343.txt", "r");
 
-      FILE* fp = fopen("dijbouti.txt", "r");
-      //FILE* fp = fopen("bcl380.txt", "r");
-      //FILE* fp = fopen("western sahara.txt", "r");
-      //FILE* fp = fopen("xqg237.txt", "r");
-      //FILE* fp = fopen("zimbabwe.txt", "r");
-      //FILE* fp = fopen("pka379.txt", "r");
-      //FILE* fp = fopen("qatar.txt", "r");
-      //FILE* fp = fopen("uruguay.txt", "r");
-      //FILE* fp = fopen("xqg131.txt", "r");
-      //FILE* fp = fopen("pma343.txt", "r");
+    //Lê numero de elementos
+    fscanf(fp, "%d\n", &nElementos);  
+    printf("Entrada:\n%d\n", nElementos);
 
-      //Lê numero de elementos
-      fscanf(fp, "%d\n", &nElementos);  
-      printf("Entrada:\n%d\n", nElementos);
+    //Lê cidades e armazena em array do tipo struct cidade
+    struct cidade cidades[nElementos];
 
-      //Lê cidades e armazena em array do tipo struct cidade
-      struct cidade cidades[nElementos];
-
-      for(int i = 0; i < nElementos; i++) {
+    for(int i = 0; i < nElementos; i++) {
         fscanf(fp, "%f ", &t);
         fscanf(fp, "%f ", &cidades[i].px);
         fscanf(fp, "%f \n", &cidades[i].py);
         //printf("%d %.2f %.2f\n", i+1, cidades[i].px, cidades[i].py); 
-      }
-
-      fclose(fp);
-      
-      printf("\n");
-
-      //Calcula distância entre cidades, armazena em matriz[][] e imprime matriz
-      float matriz[nElementos][nElementos];
-
-      //printf("Matriz gerada:\n");
-
-      for(int i = 0; i < nElementos; i++) {
-          for(int j = 0; j < nElementos; j++) {
-              matriz[i][j] = distancia(cidades[i], cidades[j]);
-              //printf("%.2f ", matriz[i][j]);
-          }
     }
+
+    fclose(fp);
+    
+    printf("\n");
+
+    //Calcula distância entre cidades, armazena em matriz[][] e imprime matriz
+    float matriz[nElementos][nElementos];
+
+    //printf("Matriz gerada:\n");
+
+    for(int i = 0; i < nElementos; i++) {
+        for(int j = 0; j < nElementos; j++) {
+            matriz[i][j] = distancia(cidades[i], cidades[j]);
+            //printf("%.2f ", matriz[i][j]);
+        }
+  }
 
     //Cria solucao gulosa inicial 
     int solucao_inicial[nElementos + 1];
     int solucao_final[nElementos + 1];
     
-    GRASP(matriz, solucao_inicial, solucao_final);
+    GRASP(matriz, solucao_inicial, solucao_final, alfa);
     printf("\n\nSolucao final GRASP: ");
     printCaminho(solucao_final);
     printf("\n\nCusto solucao final: %.2f",calculaCusto(matriz, solucao_final));
@@ -83,36 +83,73 @@ int main(void) {
     return 0;
 }
 
-void GRASP(float m[][nElementos], int* solucao_inicial, int* solucao_final){
+void GRASP(float m[][nElementos], int* solucao_inicial, int* solucao_final, int alfa){
     int improve = 0;
     int solucao_temp[nElementos];
     int otimo_local[nElementos];
-    float custoVND, custoFinal, custoGuloso, novoCusto = 1000000;
+    float custoVND, custoFinal, custoGuloso, novoCusto = INT_MAX;
 
     while(improve < 5){
-        copiaCaminho(solucao_inicial, solucao_temp);
-
+        copiaCaminho(solucao_temp, solucao_inicial);
+        
         construcaoGulosa(m, solucao_temp);
+        //pegar x candidatos da lista gulosa e o resto aleatorio;
+        gerarLCR(solucao_temp, alfa, solucao_temp);
+        
         custoGuloso = calculaCusto(m, solucao_temp);
-        custoVND = VND(m, custoGuloso, solucao_temp, otimo_local);
-  
+        printf("LCR: ");
+        printCaminho(solucao_temp);
+        
+        VND(m, custoGuloso, solucao_temp, otimo_local);
         custoFinal = calculaCusto(m, otimo_local);
 
         if(novoCusto > custoFinal){
             copiaCaminho(otimo_local, solucao_final);
+            copiaCaminho(solucao_final, solucao_temp);
             novoCusto = custoFinal;
             improve = 1;
         }
       
       improve ++;       
     }
+    
+    printf("\n\nSolução inicial gulosa: ");
+    printCaminho(solucao_temp);
+    custoGuloso = calculaCusto(m, solucao_temp);
+    printf("Custo solução inicial gulosa: %.2f\n", custoGuloso);
 
-    printf("\n\nSolucao inicial gulosa: ");
+    printf("\n\nSolução após VDN: ");
     printCaminho(solucao_temp);
     custoGuloso = calculaCusto(m, solucao_temp);
     printf("Custo solução inicial gulosa: %.2f\n", custoGuloso);
 }
 
+void gerarLCR(int* entrada, int a, int* saida){
+    int k =  nElementos / a;
+    int inseridos[nElementos];
+    int count = 0;
+
+    for(int i = 0; i < nElementos; i++){
+      inseridos[i] = 0;
+    }
+    
+    //adiciona os k primeiros elementos a solucao na ordem
+    for(int i = 0; i < k; ++i){
+        saida[i] = entrada[i];
+        inseridos[i] = 1;
+    }
+
+    //randomiza os elementos de k ao fim;
+    for(int i = k ; i < nElementos; i++){
+        int n = rand() % nElementos; 
+        
+        if(n >= k && !inseridos[n]){
+            saida[i] = entrada[n];
+            inseridos[n] = 1;
+            count++;
+        }
+    }
+}
  
 void construcaoGulosa(float m[][nElementos], int* solucao_inicial) {
     int *inseridos = malloc(nElementos * sizeof(int));
@@ -125,7 +162,7 @@ void construcaoGulosa(float m[][nElementos], int* solucao_inicial) {
     inseridos[0] = 1;
 
     for(int i = 0; i < nElementos; i++) {
-        int valor_referencia = 222222222;
+        int valor_referencia = INT_MAX;
         int vizinho_selecionado = 0;
 
         for(int j = 0; j < nElementos; j++) {
@@ -216,7 +253,7 @@ void twoOptSwap(int* entrada, int i, int k, int* saida){
     }
 }
 
-int VND(float m[][nElementos], float custo_melhor_vizinho, int* melhor_vizinho, int* otimo_local){
+void VND(float m[][nElementos], float custo_melhor_vizinho, int* melhor_vizinho, int* otimo_local){
     int improve = 0;
     int custoVND;
 
@@ -226,7 +263,6 @@ int VND(float m[][nElementos], float custo_melhor_vizinho, int* melhor_vizinho, 
         swap(m, otimo_local, otimo_local);
 
         float novoCusto = calculaCusto(m, otimo_local);
-        if(!improve){ custoVND = novoCusto;}
 
         if(novoCusto > custo_melhor_vizinho){
             copiaCaminho(melhor_vizinho, otimo_local);
@@ -237,8 +273,6 @@ int VND(float m[][nElementos], float custo_melhor_vizinho, int* melhor_vizinho, 
           improve ++;
         }
     }
-
-  return custoVND;
 }
 
 void printCaminho(int* caminho){
